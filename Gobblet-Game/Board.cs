983be704c640ffb2d700ea4e7a350a98aaba25a8
Anton <antonsamoel14@gmail.com>
2023-12-18 23:13:23 +0200

    Intial Design Created

---
+++ Gobblet-Game/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gobblet_Game
+{
+	public class Board
+	{
+		public Board(Cell[] celles)
+		{
+			Celles = celles;
+		}
+
+		public Cell[] Celles { get; set; }
+    }
+}
---
+++ Gobblet-Game/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gobblet_Game
+{
+	public class Piece
+	{
+		public Piece(int size, string playerName)
+		{
+			Size = size;
+			PlayerName = playerName;
+		}
+
+		public int Size { get; set; }
+        public string PlayerName { get; set; } = "Player 1";
+
+    }
+}
