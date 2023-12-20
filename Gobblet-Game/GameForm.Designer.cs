6564bea1cbb35af9625918c3f6d42be27fdb4cca
Anton <antonsamoel14@gmail.com>
2023-12-21 00:49:24 +0200

    Draw borad squares on the GameForm

--- Gobblet-Game/GameForm.Designer.cs
+++ Gobblet-Game/GameForm.Designer.cs
@@ -31,9 +31,9 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
 			panel1 = new Panel();
 			panel2 = new Panel();
-			panel3 = new Panel();
-			panel4 = new Panel();
 			label1 = new Label();
+			boardPanel = new Panel();
+			panel4 = new Panel();
 			label2 = new Label();
 			panel2.SuspendLayout();
 			panel4.SuspendLayout();
@@ -45,51 +45,52 @@
 			panel1.Location = new Point(0, 0);
 			panel1.Margin = new Padding(6, 5, 6, 5);
 			panel1.Name = "panel1";
-			panel1.Size = new Size(1348, 102);
+			panel1.Size = new Size(1348, 71);
 			panel1.TabIndex = 0;
 			// 
 			// panel2
 			// 
 			panel2.Controls.Add(label1);
 			panel2.Dock = DockStyle.Left;
-			panel2.Location = new Point(0, 102);
+			panel2.Location = new Point(0, 71);
 			panel2.Margin = new Padding(6, 5, 6, 5);
 			panel2.Name = "panel2";
-			panel2.Size = new Size(469, 571);
+			panel2.Size = new Size(279, 805);
 			panel2.TabIndex = 1;
-			// 
-			// panel3
-			// 
-			panel3.Dock = DockStyle.Fill;
-			panel3.Location = new Point(469, 102);
-			panel3.Margin = new Padding(6, 5, 6, 5);
-			panel3.Name = "panel3";
-			panel3.Size = new Size(879, 571);
-			panel3.TabIndex = 2;
-			// 
-			// panel4
-			// 
-			panel4.Controls.Add(label2);
-			panel4.Dock = DockStyle.Right;
-			panel4.Location = new Point(879, 102);
-			panel4.Margin = new Padding(6, 5, 6, 5);
-			panel4.Name = "panel4";
-			panel4.Size = new Size(469, 571);
-			panel4.TabIndex = 3;
 			// 
 			// label1
 			// 
 			label1.AutoSize = true;
-			label1.Location = new Point(85, 26);
+			label1.Location = new Point(70, 26);
 			label1.Name = "label1";
 			label1.Size = new Size(106, 33);
 			label1.TabIndex = 0;
 			label1.Text = "Player 1";
 			// 
+			// boardPanel
+			// 
+			boardPanel.BackColor = SystemColors.ActiveBorder;
+			boardPanel.Dock = DockStyle.Fill;
+			boardPanel.Location = new Point(279, 71);
+			boardPanel.Margin = new Padding(6, 5, 6, 5);
+			boardPanel.Name = "boardPanel";
+			boardPanel.Size = new Size(1069, 805);
+			boardPanel.TabIndex = 2;
+			// 
+			// panel4
+			// 
+			panel4.Controls.Add(label2);
+			panel4.Dock = DockStyle.Right;
+			panel4.Location = new Point(1080, 71);
+			panel4.Margin = new Padding(6, 5, 6, 5);
+			panel4.Name = "panel4";
+			panel4.Size = new Size(268, 805);
+			panel4.TabIndex = 3;
+			// 
 			// label2
 			// 
 			label2.AutoSize = true;
-			label2.Location = new Point(182, 26);
+			label2.Location = new Point(113, 26);
 			label2.Name = "label2";
 			label2.Size = new Size(106, 33);
 			label2.TabIndex = 1;
@@ -100,15 +101,18 @@
 			AutoScaleDimensions = new SizeF(15F, 31F);
 			AutoScaleMode = AutoScaleMode.Font;
 			BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-			ClientSize = new Size(1348, 673);
+			ClientSize = new Size(1348, 876);
 			Controls.Add(panel4);
-			Controls.Add(panel3);
+			Controls.Add(boardPanel);
 			Controls.Add(panel2);
 			Controls.Add(panel1);
 			Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
 			Margin = new Padding(6, 5, 6, 5);
+			MaximizeBox = false;
 			Name = "GameForm";
-			Text = "GameForm";
+			Text = "Gobblet Game";
+			Load += GameForm_Load;
 			panel2.ResumeLayout(false);
 			panel2.PerformLayout();
 			panel4.ResumeLayout(false);
@@ -120,7 +124,7 @@
 
 		private Panel panel1;
 		private Panel panel2;
-		private Panel panel3;
+		private Panel boardPanel;
 		private Panel panel4;
 		private Label label1;
 		private Label label2;
--- Gobblet-Game/GameForm.cs
+++ Gobblet-Game/GameForm.cs
@@ -16,5 +16,42 @@ namespace Gobblet_Game
 		{
 			InitializeComponent();
 		}
+		int n;
+		PictureBox[,] pictureBoxes;
+		int cellWidth = 200;
+		int cellHeight = 200;
+		private void GameForm_Load(object sender, EventArgs e)
+		{
+			n = 4;
+			pictureBoxes = new PictureBox[n, n];
+			int left = boardPanel.Left-279, top = boardPanel.Top-71;
+			Color[] colors = new Color[] { Color.White, Color.Black };
+			for (int i = 0; i < n; i++)
+			{
+				left = boardPanel.Left-279;
+				if (i % 2 == 0) { colors[0] = Color.FromArgb(112, 128, 144); colors[1] = Color.FromArgb(173, 216, 230); }
+				else { colors[0] = Color.FromArgb(173, 216, 230); colors[1] = Color.FromArgb(112, 128, 144); }
+
+				for (int j = 0; j < n; j++)
+				{
+					pictureBoxes[i, j] = new PictureBox();
+					pictureBoxes[i, j].BackColor = colors[(j % 2 == 0) ? 1 : 0];
+					pictureBoxes[i, j].Location = new Point(left, top);
+					pictureBoxes[i, j].Size = new Size(cellWidth, cellHeight);
+					left += cellWidth;
+					pictureBoxes[i, j].Name = i + " " + j;
+					//if (i < (n / 2) - 1 && P[i, j].BackColor == Color.Black) { P[i, j].Image = Properties.Resources.r; P[i, j].Name += " r"; }
+					//else if (i > (n / 2) && P[i, j].BackColor == Color.Black)
+					//{
+					//	P[i, j].Image = Properties.Resources.g; P[i, j].Name += " g";
+					//}
+					pictureBoxes[i, j].SizeMode = PictureBoxSizeMode.CenterImage;
+					boardPanel.Controls.Add(pictureBoxes[i, j]);
+				}
+				top += cellHeight;
+			}
+		}
+
+		
 	}
 }
